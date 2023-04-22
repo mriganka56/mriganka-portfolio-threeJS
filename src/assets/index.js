8bd61a9d6ab769ee0a5792f2f40a908dc0edb045
Mriganka <mrigankapauloracle@gmail.com>
2023-04-22 21:31:36 +0530

    logo updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo1 from "./logo1.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -42,7 +42,7 @@ import todo from "./todo.png"
 import tindog from "./tindog.jpg"
 
 export {
-  logo,
+  logo1,
   backend,
   creator,
   mobile,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import {navLinks} from '../consants';
-import {logo,menu,close} from '../assets';
+import {logo1,menu,close} from '../assets';
 
 const Navbar = () => {
   const[active,setActive]=useState("");
@@ -19,7 +19,7 @@ const Navbar = () => {
           setActive("");
           window.scrollTo(0,0);
         }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <img src={logo1} alt="logo1" className='w-9 h-9 object-contain'/>
           <p className="text-white text-[18px] 
           font-bold cursor-pointer flex">Mriganka Paul &nbsp; 
           <span className="sm:block hidden">| Portfolio </span></p>
